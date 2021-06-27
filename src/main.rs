6e30034eeeac36c0ad6c8ed94114db35c4cc429e
Basir Doost <basir.doost@prezi.com>
2021-06-27 23:25:23 +0200

    Refraction added

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use geometry::{Hittable, HittableList, Sphere};
 use math::Vec3;
 use color::RgbColor;
 use ray::Ray;
-use material::{Lambertian, Metal};
+use material::{Dielectric, Lambertian, Metal};
 
 fn main() {
     // Image
@@ -33,14 +33,15 @@ fn main() {
     let mut world = HittableList::new();
 
     let material_ground = Rc::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::new(Vec3::new(0.7, 0.3, 0.3)));
-    let material_left = Rc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8), 0.3));
-    let material_right = Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.9));
+    let material_center = Rc::new(Lambertian::new(Vec3::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dielectric::new(1.5));
+    let material_right = Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.1));
 
-    world.add(Rc::new(Sphere::new(Vec3::new(0.0,-100.5,-1.0), 100.0, material_ground)));
-    world.add(Rc::new(Sphere::new(Vec3::new(0.0,0.0,-1.0), 0.5, material_center)));
-    world.add(Rc::new(Sphere::new(Vec3::new(1.0,0.0,-1.0), 0.5, material_right)));
-    world.add(Rc::new(Sphere::new(Vec3::new(-1.0,0.0,-1.0), 0.5, material_left)));
+    world.add(Rc::new(Sphere::new(Vec3::new(0.0,-100.5,-1.0), 100.0, material_ground.clone())));
+    world.add(Rc::new(Sphere::new(Vec3::new(0.0,0.0,-1.0), 0.5, material_center.clone())));
+    world.add(Rc::new(Sphere::new(Vec3::new(1.0,0.0,-1.0), 0.5, material_right.clone())));
+    world.add(Rc::new(Sphere::new(Vec3::new(-1.0,0.0,-1.0), 0.5, material_left.clone())));
+    world.add(Rc::new(Sphere::new(Vec3::new(-1.0,0.0,-1.0), -0.4, material_left.clone())));
 
     // Camera
     let camera = Camera::new();
--- src/material.rs
+++ src/material.rs
@@ -1,13 +1,16 @@
+use crate::color::RgbColor;
 use crate::geometry::HitRecord;
 use crate::ray::Ray;
 use crate::math::{RandomVec, Vec3, VecApprox};
 
+use rand::Rng;
+
 pub struct Lambertian {
-    pub albedo: Vec3
+    pub albedo: RgbColor
 }
 
 impl Lambertian {
-    pub fn new(albedo: Vec3) -> Self {
+    pub fn new(albedo: RgbColor) -> Self {
         Lambertian {
             albedo
         }
@@ -15,12 +18,12 @@ impl Lambertian {
 }
 
 pub struct Metal {
-    pub albedo: Vec3,
+    pub albedo: RgbColor,
     pub fuzziness: f32
 }
 
 impl Metal {
-    pub fn new(albedo: Vec3, fuzziness: f32) -> Self {
+    pub fn new(albedo: RgbColor, fuzziness: f32) -> Self {
         Metal {
             albedo,
             fuzziness: if fuzziness < 1.0 { fuzziness } else { 1.0 }
@@ -28,12 +31,30 @@ impl Metal {
     }
 }
 
+pub struct Dielectric {
+    pub refractive_index: f32
+}
+
+impl Dielectric {
+    pub fn new(refractive_index: f32) -> Self {
+        Dielectric {
+            refractive_index
+        }
+    }
+
+    fn reflectance(cos: f32, refractive_index: f32) -> f32 {
+        // Use Schlick's approximation for reflectance
+        let r0 = ((1.0 - refractive_index) / (1.0 + refractive_index)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cos).powi(5)
+    }
+}
+
 pub trait Material {
-    fn scatter(&self, ray: &Ray, hit_rec: &HitRecord) -> Option<(Vec3, Ray)>;
+    fn scatter(&self, ray: &Ray, hit_rec: &HitRecord) -> Option<(RgbColor, Ray)>;
 }
 
 impl Material for Lambertian {
-    fn scatter(&self, _ray: &Ray, hit_rec: &HitRecord) -> Option<(Vec3, Ray)> {
+    fn scatter(&self, _ray: &Ray, hit_rec: &HitRecord) -> Option<(RgbColor, Ray)> {
         let mut scatter_direction = hit_rec.normal + Vec3::random_unit_vec();
         if scatter_direction.is_near_zero() {
             scatter_direction = hit_rec.normal;
@@ -45,7 +66,7 @@ impl Material for Lambertian {
 }
 
 impl Material for Metal {
-    fn scatter(&self, ray: &Ray, hit_rec: &HitRecord) -> Option<(Vec3, Ray)> {
+    fn scatter(&self, ray: &Ray, hit_rec: &HitRecord) -> Option<(RgbColor, Ray)> {
         let reflected = ray.direction.normalized().reflected(hit_rec.normal);
         let scattered_ray = Ray::new(hit_rec.p, reflected + self.fuzziness * Vec3::random_in_unit_sphere());
         if scattered_ray.direction.dot(hit_rec.normal) > 0.0 {
@@ -54,5 +75,27 @@ impl Material for Metal {
         } else {
             None
         }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, ray: &Ray, hit_rec: &HitRecord) -> Option<(RgbColor, Ray)> {
+        let attentuation = RgbColor::new(1.0, 1.0, 1.0);
+        let ref_ratio = if hit_rec.front_face { 1.0 / self.refractive_index } else { self.refractive_index };
+        let unit_direction = ray.direction.normalized();
+
+        let cos_theta = -unit_direction.dot(hit_rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let mut rng = rand::thread_rng();
+        let direction;
+        if ref_ratio * sin_theta > 1.0 || Self::reflectance(cos_theta, ref_ratio) > rng.gen() {
+            // Cannot refract, must reflect instead
+            direction = unit_direction.reflected(hit_rec.normal);
+        } else {
+            direction = unit_direction.refracted(hit_rec.normal, ref_ratio);
+        }
+        let scattered_ray = Ray::new(hit_rec.p, direction);
+        Some((attentuation, scattered_ray))
     }
 }
